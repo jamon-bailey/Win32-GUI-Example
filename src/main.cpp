b842e9102b9401e8078426c8f2bf60b6cafa6b78
Jamon Bailey <infinity.dev@outlook.com>
2026-06-10 21:59:57 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -15,18 +15,20 @@
  * @brief
  * Main application window class name.
  */
-static constexpr LPCSTR WINDOW_CLASS_NAME = "MainAppWindow";
+static constexpr LPCSTR APP_CLASS_NAME = "Win32 Demo";
+static constexpr LPCSTR WINDOW_TITLE_TEXT = "Demo Win32 Application";
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
 {
     // ---> Register Window Class <----------------------------------------------------------
     WNDCLASSEX windowClass{}; ///< Window class description
-    windowClass.cbSize = sizeof(WNDCLASSEX);
-    windowClass.hInstance = hInstance;
-    windowClass.lpszClassName = WINDOW_CLASS_NAME;
-    windowClass.lpfnWndProc = &windowProcedure;
-    windowClass.style = CS_HREDRAW | CS_VREDRAW;
-    windowClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+    windowClass.cbSize = sizeof(WNDCLASSEX);                    ///< Window class structure version indicator
+    windowClass.hInstance = hInstance;                          ///< Handle to module registering window class
+    windowClass.lpszClassName = APP_CLASS_NAME;                 ///< Window family (class) name
+    windowClass.lpfnWndProc = &windowProcedure;                 ///< Window class procedure function
+    windowClass.style = CS_HREDRAW | CS_VREDRAW;                ///< Window class property (style) flags
+    windowClass.hCursor = LoadCursor(NULL, IDC_ARROW);          ///< Window class cursor shape
+    windowClass.hbrBackground = GetSysColorBrush(COLOR_WINDOW); ///< Delegate minimum window repaint to Win32 API
     
     auto windowClassId = RegisterClassEx(&windowClass);
     // ----------------------------------------------------------> Register Window Class <---
@@ -37,18 +39,18 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 
     // ---> Create Window From Class <-------------------------------------------------------
     HWND windowHandle = CreateWindowEx( ///< Handle to main application window
-        NULL,                     ///< Optional window style
-        WINDOW_CLASS_NAME,        ///< Window class name
-        "Demo Win32 Application", ///< Window titlebar text
-        WS_OVERLAPPEDWINDOW,      ///< Window style
-        CW_USEDEFAULT,            ///< Window x-coordinate
-        CW_USEDEFAULT,            ///< Window y-coordinate
-        800,                      ///< Window width
-        500,                      ///< Window height
-        NULL,                     ///< Parent window handle
-        NULL,                     ///< Window menu handle
-        hInstance,                ///< Instance handle???
-        nullptr                   ///< Additional application data???
+        NULL,                ///< Extended window property (style) flags
+        APP_CLASS_NAME,      ///< Window family (class) name
+        WINDOW_TITLE_TEXT,   ///< Window titlebar text
+        WS_OVERLAPPEDWINDOW, ///< Window property (style) flags
+        CW_USEDEFAULT,       ///< Window x-coordinate
+        CW_USEDEFAULT,       ///< Window y-coordinate
+        800,                 ///< Window width
+        500,                 ///< Window height
+        NULL,                ///< Parent window handle
+        NULL,                ///< Handle to window menu
+        hInstance,           ///< Handle to module that registered window class
+        nullptr              ///< Optional data passed on window startup (WM_CREATE)
     );
     // -------------------------------------------------------> Create Window From Class <---
 
@@ -56,7 +58,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
     if (windowHandle == NULL)
         return 100;
 
-    // Set visibility of newly created window
+    // Make newly created window visible on screen
     ShowWindow(windowHandle, nCmdShow);
 
     // ---> Run Window Event Loop <----------------------------------------------------------
@@ -64,7 +66,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
     BOOL run;
 
     do {
-        // Block until next message is recieved from queue
+        // Block until next message is received from queue
         run = GetMessage(&msg, NULL, 0, 0);
 
         // Check for event loop end signal
